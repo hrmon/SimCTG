6d8d7d4659269c6a3a18e9357bf3385aa116ce1d
Hamidreza Montaseri <hr.montaseri@gandom.co>
2022-06-08 04:39:13 +0430

    Fixes on SimCTGBART

--- SimCTGEncDec/SimCTGBART/simctgbart.py
+++ SimCTGEncDec/SimCTGBART/simctgbart.py
@@ -40,9 +40,9 @@ class SimCTGBART(nn.Module):
         bsz, seqlen = input_ids.size()
         outputs = self.model(input_ids=input_ids, output_hidden_states=True, labels=labels)
         logits = outputs.logits
-        assert logits.size() == torch.Size([bsz, seqlen, self.vocab_size])
-        last_hidden_states = outputs.hidden_states[-1]
-        assert last_hidden_states.size() == torch.Size([bsz, seqlen, self.embed_dim])
+        #assert logits.size() == torch.Size([bsz, seqlen, self.vocab_size])
+        last_hidden_states = outputs.decoder_hidden_states[-1]
+        #assert last_hidden_states.size() == torch.Size([bsz, seqlen, self.embed_dim])
         return last_hidden_states, logits
 
     def eval_loss(self, input_ids, labels):
